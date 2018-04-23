b9d2697d8ae35223b72c1539fb6ede03db967a00
jls <1454065462@qq.com>
2018-04-23 13:40:59 +0800

    delete myProject/Nuxt

--- Nuxt/pages/news/_id.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <p>{{$route.params.id}}</p>
-    <nuxt-link :to="{name:'index'}">Home</nuxt-link>
-  </div>
-</template>
--- Nuxt/pages/user/_id.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>这是id为{{$route.params.id}}的用户的详情</h1>
-    <br>
-    <div>
-      <nuxt-link :to="{name:'user'}">返回用户列表</nuxt-link>
-    </div>
-  </div>
-</template>
